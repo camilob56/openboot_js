7cd6e4487fcdf9a18b8674dad851af55cda2d3e1
camilob56 <cristianc-56@hotmail.com>
2022-09-22 20:54:36 -0500

    Agrego ejercicio 2

---
+++ Ejercicio 2/ejercicio_2.js
@@ -0,0 +1,8 @@
+const librofav = {
+    titulo: "El coronel no tiene quien le escriba",
+    autor: "Gabriel García Márquez",
+    fecha: 1986,
+    url: "https://www.amazon.com/-/es/Gabriel-Garc%C3%ADa-M%C3%A1rquez/dp/0307475441"
+}
+const arreglo = new Array("Camilo", 28, true, new Date("may 01 1994"), librofav);
+console.log(arreglo);
