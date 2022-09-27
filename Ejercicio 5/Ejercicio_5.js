28c87381e3ecb2ce6db78bb3e86f01cd8da12d03
camilob56 <cristianc-56@hotmail.com>
2022-09-27 17:19:22 -0500

    Ejercicio 5 numbers

---
+++ Ejercicio 5/Ejercicio_5.js
@@ -0,0 +1,14 @@
+
+let alturaCm = 163;
+let alturaM = 1.63;
+let peso = 62.5;
+
+console.log(Math.ceil(alturaM)); 
+
+console.log(Math.floor(peso));
+
+let numMax = Infinity == (Infinity +1);
+console.log(numMax);
+
+let maxvalor = Number.MAX_VALUE == (Number.MAX_VALUE +1);
+console.log(maxvalor);
