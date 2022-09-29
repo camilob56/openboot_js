f328589c400b87d14f1bb8ab40aaa4d83e8a119f
camilob56 <cristianc-56@hotmail.com>
2022-09-29 15:29:53 -0500

    Ejercicio 7 - conjuntos

---
+++ Ejercicio 7/Conjuntos.js
@@ -0,0 +1,10 @@
+const familia = new Array('Camilo', 'Laura', 'Yamile', 'Orlando');
+const familiaSet = new Set(familia);
+
+console.log(familiaSet);
+
+familiaSet.add('Camilo');
+console.log(familiaSet);
+
+familiaSet.add('JavaScript');
+console.log(familiaSet);
