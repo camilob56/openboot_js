3337df204ff80e04e6095b1a244e2a58be7b1637
camilob56 <cristianc-56@hotmail.com>
2023-02-17 17:20:26 -0500

    Ejercicio 15 - persistencia de datos 2

--- Ejercicio 15/index.js
+++ Ejercicio 15/index.js
@@ -12,3 +12,6 @@ JSON.parse(sessionStorage.getItem('persona'));
 
 localStorage.setItem('persona', JSON.stringify(persona));
 JSON.parse(localStorage.getItem('persona'));
+
+const nuevo = new Date();
+document.cookie = `persona=${JSON.stringify(persona)};expires=${new Date(nuevo.getTime() + 2 * 60000)}`
